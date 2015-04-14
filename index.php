5f16cb4e7cca4ccdcf4c9a65d8ef379f01bddfe0
Mike Riley <mikeriley131@gmail.com>
2015-04-13 21:16:43 -0400

    Amended initial commit

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <html>
 	<head
 		<meta charset="UTF-8">
-		<meta name="description" content="">
+		<meta name="description" content="Ms. Sams...">
 		<meta name="author" content="Mike Riley">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-		<title></title>
+		<title>Go to Ms. Sams</title>
 		
 		<link rel="stylesheet" href="assets/css/main.min.css" />
 
-		<script src="assets/js/main.min.js"></script>
+		<!-- <script src="assets/js/main.min.js"></script> -->
 
 		<!--[if lt IE 9]>
 		<script src="http://html5shiv-printshiv.googlecode.com/svn/trunk/html5shiv-printshiv.js"></script>
@@ -19,6 +19,6 @@
 	</head>
 
 	<body class="gridlock">
-		
+		<p>Hello world.</p>
 	</body>
 </html>
