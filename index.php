78ebb188cdbeea36e0aa064c1f4eeaa58f386339
Mike Riley <mikeriley131@gmail.com>
2015-04-29 23:06:40 -0400

    Started interior template

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta name="author" content="Mike Riley">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-		<title>Go to Ms. Sams</title>
+		<title>Go to Ms. Sams - home</title>
 		
 		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="_assets/css/main.min.css" />
@@ -19,111 +19,115 @@
 		<![endif]-->
 	</head>
 
-	<body class="gridlock home">
-		<header class="row">
-			<div class="logo max-3 desktop-3 tablet-1 mobile-1">
-				<a href="/"><img src="_assets/images/go-to-ms-sams-logo.svg" alt="Go To Ms. Sams logo"></a>
-			</div>
-			<nav class="max-9 desktop-9 tablet-5 mobile-1 mobile-push-1 min-1 min-push-1 nav-collapse">
-				<ul>
-					<li><a href="">About Ms. Sams</a></li>
-					<li><a href="">What We Do</a></li>
-					<li><a href="">Blog</a></li>
-					<li><a href="">Contact</a></li>
-					<li>
-						<ul class="social">
-							<li><a class="fb" href="">Facebook</a></li>
-							<li><a class="inst" href="">Instagram</a></li>
-							<li><a class="li" href="">Linked In</a></li>
-							<li><a class="twit" href="">Twitter</a></li>
+	<body>
+		<div class="gridlock home">
+			<header>
+				<div class="row">
+					<div class="logo max-3 desktop-3 tablet-1 mobile-1">
+						<a href="/"><img src="_assets/images/go-to-ms-sams-logo.svg" alt="Go To Ms. Sams logo"></a>
+					</div>
+					<nav class="max-9 desktop-9 tablet-5 mobile-1 mobile-push-1 min-2 min-push-1 nav-collapse">
+						<ul>
+							<li><a href="">About Ms. Sams</a></li>
+							<li><a href="">What We Do</a></li>
+							<li><a href="">Blog</a></li>
+							<li><a href="">Contact</a></li>
+							<li>
+								<ul class="social">
+									<li><a class="fb" href="">Facebook</a></li>
+									<li><a class="inst" href="">Instagram</a></li>
+									<li><a class="li" href="">Linked In</a></li>
+									<li><a class="twit" href="">Twitter</a></li>
+								</ul>
+							</li>
 						</ul>
-					</li>
-				</ul>
-			</nav>
-		</header>
-		<section class="hero-image">
-			<div class="row">
-				<p>Making meaningful connections with young people.</p>
-			</div>
-			<img src="_assets/images/go-to-ms-sams-homepg-feature.jpg" alt="">
-		</section>
-		<section class="hero-text">
-			<div class="row">
-				<p>Go to Ms.Sams consults with non-profits, schools and the business community to create structures that promote meaningful and impactful relationships with youth.</p>
-			</div>
-		</section>
-		<section class="what-we-do bordered">
-			<div class="row">
-				<h2>What we do:</h2>
-				<div class="max-4 desktop-4 contained">
-					<h3>School Climate &amp; Culture</h3>
-					<p>Creating a school culture that fosters a positive and productive learning environment.</p>
+					</nav>
 				</div>
-				<div class="max-4 desktop-4 contained">
-					<h3>Mentoring</h3>
-					<p>Using best practices that establish deep connections to youth.</p>
+			</header>
+			<section class="hero-image">
+				<div class="row">
+					<h1>Making meaningful connections with young people.</h1>
 				</div>
-				<div class="max-4 desktop-4 contained">
-					<h3>School &amp; Business Partnerships</h3>
-					<p>Connecting schools and the business community for early career exposure and experiential learning opportunities.</p>
+				<img src="_assets/images/go-to-ms-sams-homepg-feature.jpg" alt="">
+			</section>
+			<section class="hero-text">
+				<div class="row">
+					<p>Go to Ms.Sams consults with non-profits, schools and the business community to create structures that promote meaningful and impactful relationships with youth.</p>
 				</div>
-			</div>
-		</section>
-		<section class="our-process bordered">
-			<div class="row">
-				<h2>Our process:</h2>
-				<div class="process-number">
-					<span id="one">1</span>
-					<div class="reveal wow animated fadeInDown one">
-						<img src="_assets/images/icon-ear.svg" alt="ear icon">
-						<p>Listen &amp; Assess</p>
+			</section>
+			<section class="what-we-do bordered">
+				<div class="row">
+					<h2>What we do:</h2>
+					<div class="max-4 desktop-4 contained">
+						<a href="#"><h3>School Climate &amp; Culture</h3></a>
+						<a href="#"><p>Creating a school culture that fosters a positive and productive learning environment.</p></a>
+					</div>
+					<div class="max-4 desktop-4 contained">
+						<a href="#"><h3>Mentoring</h3></a>
+						<a href="#"><p>Using best practices that establish deep connections to youth.</p></a>
+					</div>
+					<div class="max-4 desktop-4 contained">
+						<a href="#"><h3>School &amp; Business Partnerships</h3></a>
+						<a href="#"><p>Connecting schools and the business community for early career exposure and experiential learning opportunities.</p></a>
 					</div>
 				</div>
-				<div class="process-number">
-					<span>2</span>
-					<div class="reveal wow animated fadeInDown two">
-						<img src="_assets/images/icon-design.svg" alt="design icon">
-						<p>Design &amp; Develop</p>
+			</section>
+			<section class="our-process bordered">
+				<div class="row">
+					<h2>Our process:</h2>
+					<div class="process-number">
+						<span id="one">1</span>
+						<div class="reveal wow animated fadeInDown one">
+							<img src="_assets/images/icon-ear.svg" alt="ear icon">
+							<p>Listen &amp; Assess</p>
+						</div>
+					</div>
+					<div class="process-number">
+						<span>2</span>
+						<div class="reveal wow animated fadeInDown two">
+							<img src="_assets/images/icon-design.svg" alt="design icon">
+							<p>Design &amp; Develop</p>
+						</div>
+					</div>
+					<div class="process-number">
+						<span>3</span>
+						<div class="reveal wow animated fadeInDown three">
+							<img src="_assets/images/icon-chart.svg" alt="chart icon">
+							<p>Evaluate &amp; Implement</p>
+						</div>
+					</div>
+					<div class="process-number">
+						<span>4</span>
+						<div class="reveal wow animated fadeInDown four">
+							<img src="_assets/images/icon-pin-point.svg" alt="pin point icon">
+							<p>Reflect &amp; Revisit</p>
+						</div>
 					</div>
 				</div>
-				<div class="process-number">
-					<span>3</span>
-					<div class="reveal wow animated fadeInDown three">
-						<img src="_assets/images/icon-chart.svg" alt="chart icon">
-						<p>Evaluate &amp; Implement</p>
-					</div>
+			</section>
+			<section class="contact">
+				<div class="row">
+					<h2 class="max-2">Contact:</h2>
+					<form action="" class="max-10">
+						<ul>
+							<li><input type="text" id="name" placeholder="Your Name" required></li>
+							<li><input type="email" id="email" placeholder="Your Email" required></li>
+							<li class="phone"><input type="tel" id="phone" placeholder="Phone No."></li>
+							<li class="city"><input type="text" id="city" placeholder="City" required></li>
+							<li><textarea id="message" required>Your Message</textarea></li>
+							<li><input type="submit" id="submit" value="Submit"></li>
+						</ul>
+					</form>
 				</div>
-				<div class="process-number">
-					<span>4</span>
-					<div class="reveal wow animated fadeInDown four">
-						<img src="_assets/images/icon-pin-point.svg" alt="pin point icon">
-						<p>Reflect &amp; Revisit</p>
-					</div>
+			</section>
+			<footer>
+				<div class="row">
+					<p>Making meaningful connections with young people. &nbsp; &copy;<?php echo date('Y') ?> Jamila Sams</p>
 				</div>
-			</div>
-		</section>
-		<section class="contact">
-			<div class="row">
-				<h2 class="max-2">Contact:</h2>
-				<form action="" class="max-10">
-					<ul>
-						<li><input type="text" id="name" placeholder="Your Name" required></li>
-						<li><input type="email" id="email" placeholder="Your Email" required></li>
-						<li class="phone"><input type="text" id="phone" placeholder="Phone No."></li>
-						<li class="city"><input type="text" id="city" placeholder="City" required></li>
-						<li><textarea id="message" required>Your Message</textarea></li>
-						<li><input type="submit" id="submit"></li>
-					</ul>
-				</form>
-			</div>
-		</section>
-		<footer>
-			<div class="row">
-				<p>Making meaningful connections with young people. &nbsp; &copy;<?php echo date('Y') ?> Jamila Sams</p>
-			</div>
-		</footer>
-		<script>
-		  var nav = responsiveNav(".nav-collapse");
-		</script>
+			</footer>
+			<script>
+			  var nav = responsiveNav(".nav-collapse");
+			</script>
+		</div>
 	</body>
 </html>
---
+++ interior.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head
+		<meta charset="UTF-8">
+		<meta name="description" content="">
+		<meta name="author" content="Mike Riley">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+		<title>Go to Ms. Sams - interior</title>
+
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="_assets/css/main.min.css" />
+
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv-printshiv.googlecode.com/svn/trunk/html5shiv-printshiv.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<div class="gridlock interior">
+			<header>
+				<div class="row">
+					<div class="logo max-2 desktop-2 tablet-1 mobile-1">
+						<a href="/"><img src="_assets/images/go-to-ms-sams-logo.svg" alt="Go To Ms. Sams logo"></a>
+					</div>
+					<nav class="max-9 max-push-1 desktop-9 desktop-push-1 tablet-5 mobile-1 mobile-push-1 min-2 min-push-1 nav-collapse">
+						<ul>
+							<li><a href="">About Ms. Sams</a></li>
+							<li><a href="">What We Do</a></li>
+							<li><a href="">Blog</a></li>
+							<li><a href="">Contact</a></li>
+							<li>
+								<ul class="social">
+									<li><a class="fb" href="">Facebook</a></li>
+									<li><a class="inst" href="">Instagram</a></li>
+									<li><a class="li" href="">Linked In</a></li>
+									<li><a class="twit" href="">Twitter</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+			<div class="row">
+				<aside class="max-3 desktop-3 tablet-2 mobile-3">
+					<blockquote>
+						<p>"Jamila envisions, designs and implements school-based initiatives that support an inclusive, dynamic school culture. Jamila possesses an informed understanding of the needs of urban youth along with the skill to collaboratively revise and improve structures over time. She is a talented and tested educator who brings deep experience and hard-earned wisdom."</p>
+						<cite>Mark Conrad, Chief Schools Officer, Expeditionary Learning on LinkedIn</cite>
+					</blockquote>
+
+					<blockquote>
+						<p>"Jamila is equally talented in the areas of commitment, dedication, enthusiasm, strategy, and vision relating to moving youth forward. I've known Jamila for more than a decade and her hat has always been placed in the capacity of serving without fear or reservation around gaining a result. She makes a mark in all that she does."</p>
+						<cite>Torin Ellis, Diversity Search Maverick / Interview Architect / Hybrid Recruiter  on LinkedIn</cite>
+					</blockquote>
+				</aside>
+				<section class="content max-9 desktop-9 tablet-4 mobile-3">
+					<h1>School Climate &amp; Culture</h1>
+					<p class="intro">How much time do school leaders spend on effective instructional leadership? 50%? 75%? According to a Wallace Foundation study, School leaders spend on average 30% on instruction and 70% on issues such as student behavioral infractions, budgets and other operational needs. Many school leaders find themselves being reactive to various non-instructional issues instead of proactively creating, maintaining and re-evaluating their school climate and values.</p>
+
+					<p class="intro">Go To Ms. Sams's works closely with school leaders to create optimal learning environments where every stakeholder is on one accord for student achievement. Through inclusive, fair and concise school-wide structures grounded in data, we work collaboratively to design evidence-based systems that promote a positive and productive culture of learning for all.</p>
+
+					<h2>Creating a School & Community Value System</h2>
+					<p>Creating a school and community value system starts with thinking about the innate values you want every stakeholder in your building to promote and uphold. How are values integrated and measured? What structures are in place to provide support for these values to thrive? This workshop helps school leaders to align their vision for school climate and culture with supporting structures that promote a safe, positive and productive learning environment.</p>
+
+					<h2>The Power of Advisory</h2>
+					<p>How well do the adults in your building know the students? How well do students know each other? Advisory is a sacred time of the day where adults and students meet to build positive connections and meet students' inherent social and academic needs while providing the space for discovering new individual talents and abilities. We will work closely to ensure that advisory time is being used efficiently and in a way that promotes your school’s values.</p>
+
+					<h2>Parent Engagement</h2>
+					<p>Parent engagement is essential for the success of a school community. Even the most disengaged parents can be engaged when the culture of the school is inviting and understanding of parents' needs and desires to get involved. How are parents and members of the community engaged? How are they welcomed? Are they an active part of the decision making process of your school? Do they have a voice? Are your staff members in tuned with the cultures and backgrounds of the families they serve? Do you use parent participation data to drive parent engagement and student learning? This workshop challenges school leaders develop an inclusive and interactive parent engagement model for their school.</p>
+
+					<h2>Home Visits</h2>
+					<p>Although students spend over half of learning years in school, educators only see the "tip" of the iceberg. What happens in students’ lives at home has transformative effects on their learning. A growing number of schools are creating more meaningful collaborations with parents. These schools embrace a philosophy of partnership that goes well beyond the one-way and often discipline-related communications from teachers to parents. 
+					When schools adopt a deepened trust between parents and teachers and regular sharing of their respective knowledge and understanding of students’ needs, interests and progress, children are more likely to succeed. Home visits help to bridge the gap between home and school. This workshop will enable school leaders to create a strategic plan around home visits that will allow for a more cohesive school community.</p>
+
+					<h2>Peer-to-Peer Youth Development</h2>
+					<p>By providing opportunities for youth to create positive change in their school, students invest in their own learning community. Peer organizations allow for students to share information and perspectives while gaining experiential practice in their role as leaders.  We will work with your school’s identified peer leaders to become powerful catalysts for change in their schools and communities.</p> 
+
+					<h2>Student Led Conferences</h2>
+					<p>How does your learning community celebrate student learning based on their individual growth? When students are active participants in their assessment process, they have an opportunity to increase their intrinsic motivation, which fosters growth. Student Led Conferences places students at the center of their learning. This workshop helps schools design and implement structures that support student led conferences, from establishing a standards-based grading scale to collecting, reflecting and presenting student work.</p>
+
+					<h2>Cultural Competency Training for Urban Educators</h2>
+					<p>As a country, we benefit from the education of ALL children. However, how do we not only make learning relevant, but transformational for the communities in which students come from? Cultural competence provides a set of skills that professionals need in order to improve practice to serve all students and communicate effectively with their families. This workshop empowers educators to build on the cultural and language qualities that young people bring to the classroom rather than viewing those qualities as deficits.</p>
+				</section>
+			</div>
+		</div>
+	</body>
+</html>
